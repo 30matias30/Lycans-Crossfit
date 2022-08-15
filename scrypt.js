f34ecb7aa11194d1ad62462a30adf418266f0df8
Matias Astorga <30matias30@gmail.com>
2022-08-14 23:15:37 -0300

    Casi Final

--- scrypt.js
+++ scrypt.js
@@ -2,7 +2,7 @@ let btnLeft = document.getElementById('btn-left');
 let btnRight = document.getElementById('btn-right');
 let bodyy = document.body;
 
-let num = 2;
+let num = 0;
 let contador;
 let bandera = false;
 
@@ -62,7 +62,7 @@ function timerFotos() {
 
   console.log(num);
 
-  setTimeout(timerFotos, 1000000);
+  setTimeout(timerFotos, 5000);
 
 }
 
